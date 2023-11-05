bb2a6fbfe074e83817b9a09bf9efcf2ff72f5e88
Eryani <mym.aleryani@gmail.com>
2023-11-05 23:34:54 +0100

    added a text that tells if there are no favorite events

--- src/stores/useEventsStore.js
+++ src/stores/useEventsStore.js
@@ -13,8 +13,7 @@ export const useEventsStore = defineStore('events', {
     async fetchEvents({keyword='', id=null}={}) {
 
       const url = `https://app.ticketmaster.com/discovery/v2/events.json?size=21${keyword!=''?`&keyword=${keyword}`:''}&${id!=null?`id=${id}&`:''}countryCode=NL&apikey=${apiKey}`
-      console.log(url);
-     
+
       return await fetch(url)
         .then((res) => res.json())
         .then((result) => (this.events = result?._embedded?.events))
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -7,8 +7,10 @@ const favoritesStore = useFavoritesStore()
 </script>
 
 <template >
+
     <div class="flex flex-col justify-between h-screen">
         <Header />
+        <p class="text-center text-xl" v-if="favoritesStore.favorites.length===0"> There is no favorite events</p>
 
         <Events :events="favoritesStore.favorites" class="grid grid-cols-3 gap-4  w-10/12 px-8 mb-5 mx-auto" />
         <Footer />
